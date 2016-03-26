b9caf256c1f367e7fcb5c2a60e111dabef7472d6
Valentin Korolev <kor.val@inbox.ru>
2016-03-26 13:46:36 +0300

    Group list refactoring

--- app/web/scripts/controllers/group/group-files-upload.js
+++ app/web/scripts/controllers/group/group-files-upload.js
@@ -1,87 +1,88 @@
 (function(angular) {
     'use strict';
     angular.module('C3web.controllers')
-        .controller('fileUploadController', ['$scope', '$routeParams', 'FileUploader', '$sce', function($scope, $routeParams, FileUploader, $sce) {
-            var uploader = $scope.uploader = new FileUploader({
-                url: '/api/file'
-            });
-            var currentPath = $sce.trustAsResourceUrl($routeParams.path);
-            // FILTERS
+        .controller('fileUploadController', ['$scope', '$routeParams', '$http', 'FileUploader', '$sce',
+            function($scope, $routeParams, $http, FileUploader, $sce) {
+                $scope.model = {
+                    tags: []
+                };
 
-            uploader.filters.push({
-                name: 'customFilter',
-                fn: function(item /*{File|FileLikeObject}*/ , options) {
-                    return this.queue.length < 10;
-                }
-            });
+                var uploader = $scope.uploader = new FileUploader({
+                    url: '/api/file'
+                });
+                var currentPath = $sce.trustAsResourceUrl($routeParams.path);
+                // FILTERS
 
-            // CALLBACKS
-
-            uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/ , filter, options) {
-                console.info('onWhenAddingFileFailed', item, filter, options);
-            };
-            uploader.onAfterAddingFile = function(fileItem) {
-                console.info('onAfterAddingFile', fileItem);
-            };
-            uploader.onAfterAddingAll = function(addedFileItems) {
-                console.info('onAfterAddingAll', addedFileItems);
-            };
-            uploader.onBeforeUploadItem = function(item) {
-                console.info('onBeforeUploadItem', item);
-                item.formData.push({
-                    url: "/" + currentPath + "/" + item.file.name
-                });
-                item.formData.push({
-                    fileName: item.file.name
-                });
-                item.formData.push({
-                    fileSize: item.file.size
-                });
-                item.formData.push({
-                    fileTags: ""
-                });
-                item.formData.push({
-                    fileType: "Other"
-                });
-                item.formData.push({
-                    contentType: item.file.type
-                });
-                item.formData.push({
-                    isFolder: false
-                });
-            };
-            uploader.onProgressItem = function(fileItem, progress) {
-                console.info('onProgressItem', fileItem, progress);
-            };
-            uploader.onProgressAll = function(progress) {
-                console.info('onProgressAll', progress);
-            };
-            uploader.onSuccessItem = function(fileItem, response, status, headers) {
-                console.info('onSuccessItem', fileItem, response, status, headers);
-
-                $.ajax({
-                    url: "/api/annotation/" + encodeURIComponent("/" + currentPath + "/" + fileItem.file.name),
-                    cache: false
-                }).done(function(response) {
-                    console.info('OnAnnotaitonDone', response);
-                    $scope.tags = response;
-                    $('input').focus();
+                uploader.filters.push({
+                    name: 'customFilter',
+                    fn: function(item /*{File|FileLikeObject}*/ , options) {
+                        return this.queue.length < 10;
+                    }
                 });
 
-            };
-            uploader.onErrorItem = function(fileItem, response, status, headers) {
-                console.info('onErrorItem', fileItem, response, status, headers);
-            };
-            uploader.onCancelItem = function(fileItem, response, status, headers) {
-                console.info('onCancelItem', fileItem, response, status, headers);
-            };
-            uploader.onCompleteItem = function(fileItem, response, status, headers) {
-                console.info('onCompleteItem', fileItem, response, status, headers);
-            };
-            uploader.onCompleteAll = function() {
-                console.info('onCompleteAll');
-            };
+                // CALLBACKS
 
-            console.info('uploader', uploader);
-        }]);
+                uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/ , filter, options) {
+                    console.info('onWhenAddingFileFailed', item, filter, options);
+                };
+                uploader.onAfterAddingFile = function(fileItem) {
+                    console.info('onAfterAddingFile', fileItem);
+                };
+                uploader.onAfterAddingAll = function(addedFileItems) {
+                    console.info('onAfterAddingAll', addedFileItems);
+                };
+                uploader.onBeforeUploadItem = function(item) {
+                    console.info('onBeforeUploadItem', item);
+                    item.formData.push({
+                        url: "/" + currentPath + "/" + item.file.name
+                    });
+                    item.formData.push({
+                        fileName: item.file.name
+                    });
+                    item.formData.push({
+                        fileSize: item.file.size
+                    });
+                    item.formData.push({
+                        fileTags: ''
+                    });
+                    item.formData.push({
+                        fileType: 'Other'
+                    });
+                    item.formData.push({
+                        contentType: item.file.type
+                    });
+                    item.formData.push({
+                        isFolder: false
+                    });
+                };
+                uploader.onProgressItem = function(fileItem, progress) {
+                    console.info('onProgressItem', fileItem, progress);
+                };
+                uploader.onProgressAll = function(progress) {
+                    console.info('onProgressAll', progress);
+                };
+                uploader.onSuccessItem = function(fileItem, response, status, headers) {
+                    console.info('onSuccessItem', fileItem, response, status, headers);
+
+                    $http({
+                        url: '/api/annotation/' + encodeURIComponent("/" + currentPath + "/" + fileItem.file.name),
+                        method: 'GET'
+                    }).then(function(result) {
+                        $scope.model.tags = result.data;
+                    });
+                };
+                uploader.onErrorItem = function(fileItem, response, status, headers) {
+                    console.info('onErrorItem', fileItem, response, status, headers);
+                };
+                uploader.onCancelItem = function(fileItem, response, status, headers) {
+                    console.info('onCancelItem', fileItem, response, status, headers);
+                };
+                uploader.onCompleteItem = function(fileItem, response, status, headers) {
+                    console.info('onCompleteItem', fileItem, response, status, headers);
+                };
+                uploader.onCompleteAll = function() {
+                    console.info('onCompleteAll');
+                };
+            }
+        ]);
 })(angular);
